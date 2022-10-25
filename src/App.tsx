6a6f930f054f2c915b27da92cac70841a10154a1
ilyakurachkin <patlatov@inbox.ru>
2022-10-25 20:49:21 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,25 @@ import {Button} from "./components/Button";
 
 function App() {
 
-    const ButtonFooOne =(subscriber:string)=> {
+    const ButtonFooOne = (subscriber: string) => {
         console.log(subscriber)
     }
 
-    const ButtonFooSecond =(subscriber:string)=> {
+    const ButtonFooSecond = (subscriber: string) => {
         console.log(subscriber)
     }
 
-    const ButtonFooThree =()=> {
+    const ButtonFooThree = () => {
         console.log('im Stupid Button')
     }
 
-    const ButtonTest =(subscribe:string)=> {
-        console.log(subscribe)
-    }
 
     return (
         <>
-            <Button name={'MyYoutubeChanel-1'} callBack={()=>ButtonFooOne('im Ilya')}/>
+            <Button  name={'MyYoutubeChanel-1'} callBack={()=>ButtonFooOne('im Ilya')}/>
             <Button name={'MyYouTubeChanel-2'} callBack={()=>ButtonFooSecond('im Ivan')}/>
             <Button name={'MyYouTubeChanel-3'} callBack={ButtonFooThree}/>
-            <Button name={'test'} callBack={()=>ButtonTest('tessst')}/>
+
         </>
     )
 }
